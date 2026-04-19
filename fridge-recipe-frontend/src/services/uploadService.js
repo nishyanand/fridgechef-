c12b3aa885d7a3fa2c561b7596cea4dd83b0e29b
Nisha Anand <anandnisha277@gmail.com>
2026-04-19 19:55:53 +0530

    updated backend URL for production

--- fridge-recipe-frontend/src/services/uploadService.js
+++ fridge-recipe-frontend/src/services/uploadService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://fridgechef-backend-3ylm.onrender.com/api';
 
 export const uploadAndAnalyze = async (imageFile, onProgress) => {
   const formData = new FormData();
